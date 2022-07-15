c19d7934fec90dbb9c89e4faf5e6ff120c9484d7
Faraz Ahmed <farazahmediu01@gmail.com>
2022-07-15 13:00:15 +0500

    Create DataType.r

---
+++ 7-15-22/DataType.r
@@ -0,0 +1,26 @@
+# Data Types
+
+a = 10   # numeric 
+b = 10.5 # numeric
+c = 12L  #integer
+special_type_of_numbers = 7+5i # Complex = Real number + imaginary Numbers
+name = "Ali"
+
+t = TRUE # boolean
+f = FALSE # boolean 
+
+print(class(a))
+print(class(b))
+print(class(c))
+print(class(complex))
+
+print(class(name))
+print(class(t))
+print(class(f))
+print(class(b)) 
+
+
+# Short Keys
+# ctrl + shift +c  Shortkey to comment a line in r studio
+# ctrl + enter to run all selected lines
+# ctrl + l to clear terminal
